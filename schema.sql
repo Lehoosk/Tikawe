77c95b98556d21809f27b813b0a899ea078401c0
Oskari Leho <oskari.leho@helsinki.fi>
2025-06-29 13:32:24 +0300

    added ON CASCADE conditions and 1-rep max table

--- schema.sql
+++ schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE users (
 
 CREATE TABLE exercises (
     id INTEGER PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    exercise_type_id INTEGER REFERENCES exercise_types,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    exercise_type_id INTEGER REFERENCES exercise_types ON DELETE CASCADE,
     exercise_class_id INTEGER REFERENCES classes,
     exercise_weight REAL,
     exercise_date TEXT,
@@ -29,21 +29,31 @@ CREATE TABLE exercise_types (
 CREATE TABLE classes (
     id INTEGER PRIMARY KEY,
     label TEXT,
-    sets INTEGER,
-    reps  INTEGER,
-    alpha REAL
+    reps  INTEGER
 );
 
 INSERT INTO classes
 VALUES
- (1, '3 series of 2 reps', 3, 2, 0.35),
- (2, '5 series of 5 reps', 5, 5, 0.20),
- (3, '3 series of 10 reps', 3, 10, 0.10);
+ (1, '3 series of 2 reps', 2),
+ (2, '5 series of 5 reps', 5),
+ (3, '3 series of 10 reps', 10);
+
+CREATE TABLE pr_records (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    exercise_type_id INTEGER REFERENCES exercise_types ON DELETE CASCADE,
+    exercise_class_id INTEGER REFERENCES classes,
+    e1rm_epley REAL,
+    e1rm_lombardi REAL,
+    e1rm_brzycki REAL,
+    ex_weight REAL,
+    pr_date TEXT
+);
 
  CREATE TABLE comments (
     id INTEGER PRIMARY KEY,
-    exercise_id INTEGER REFERENCES exercises,
-    user_id INTEGER REFERENCES users,
+    exercise_id INTEGER REFERENCES exercises ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
     comment_text TEXT,
     created_date TEXT
 );
